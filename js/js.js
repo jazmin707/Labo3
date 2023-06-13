93d7e62105ac31e870309ed71a1825ad758e2db1
jazmin707 <jazcar2003@gmail.com>
2023-06-13 15:55:49 -0300

    arreglo index

---
+++ js/js.js
@@ -0,0 +1,5 @@
+const boton_menu= document.querySelector(".boton_menu");
+const navMenu= document.querySelector(".menu");
+
+boton_menu.addEventListener("click", () =>{
+    navMenu.classList.toggle("nav-menu-visible")});
