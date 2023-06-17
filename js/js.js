3d8b89bb70b09f679816082b501b77a7c9c90918
jazmin707 <jazcar2003@gmail.com>
2023-06-16 21:51:13 -0300

    formulario funcional

--- js/js.js
+++ js/js.js
@@ -1,6 +1,21 @@
 const boton_menu= document.querySelector(".boton_menu");
 const navMenu= document.querySelector(".menu");
 
-boton_menu.addEventListener("click", () =>{
-    navMenu.classList.toggle("nav-menu-visible")});
 
+boton_menu.addEventListener("click", () =>{
+    navMenu.classList.toggle("nav-menu-visible")
+});
+
+
+
+const $form = document.querySelector('#form')
+const $buttonMailto = document.querySelector('#conectar')
+
+$form.addEventListener('submit', handleSubmit)
+
+function handleSubmit(event) {
+    event.preventDefault()
+    const form = new FormData(this)
+    $buttonMailto.setAttribute('href', `mailto:jazcar2003@gmail.com?subject=nombre ${form.get('name')}  correo ${form.get('email')}&body=${form.get('message')}`)
+    $buttonMailto.click()
+}
