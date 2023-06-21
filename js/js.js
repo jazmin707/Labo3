0dfe0e67b84c02105d435621eed13abe00e1571f
jazmin707 <jazcar2003@gmail.com>
2023-06-21 17:36:02 -0300

    formulario terminado(?

--- js/js.js
+++ js/js.js
@@ -6,16 +6,3 @@ boton_menu.addEventListener("click", () =>{
     navMenu.classList.toggle("nav-menu-visible")
 });
 
-
-
-const $form = document.querySelector('#form')
-const $buttonMailto = document.querySelector('#conectar')
-
-$form.addEventListener('submit', handleSubmit)
-
-function handleSubmit(event) {
-    event.preventDefault()
-    const form = new FormData(this)
-    $buttonMailto.setAttribute('href', `mailto:jazcar2003@gmail.com?subject=nombre ${form.get('name')}  correo ${form.get('email')}&body=${form.get('message')}`)
-    $buttonMailto.click()
-}
